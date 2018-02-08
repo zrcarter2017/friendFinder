da56549a00a10d10bdd30075042dae73305c1f6f
zrcarter2017 <zcarter@allegiancy.us>
2018-02-08 00:22:04 -0500

    fixed id, fixed submit button, and made changes to routes

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,9 +1,13 @@
+var surveyData = require("../data/friends");
+
+module.exports = function(app) {
+
 app.get("/api/friends", function(req, res) {
   res.json(friends);
 });
 
 app.post("/api/friends", function(req, res) {
-	var newfriend = req.body;
-	console.log(newfriend);
-	friends.push(newfriend);
+	console.log(res);
 });
+
+};
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -4,8 +4,6 @@ app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 
-
-
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/home.html"));
 });
