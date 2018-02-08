5ae055883b1ae7af90dadb7e73dfd846c0c8d976
zrcarter2017 <zcarter@allegiancy.us>
2018-02-07 23:06:09 -0500

    added data in friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,163 @@
-friends = [];
+friends = [
+{
+  "name":"Bob",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      3,
+      3,
+      3,
+      5,
+      1,
+      3,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"Joe",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      2,
+      1,
+      2,
+      2,
+      4,
+      1
+    ]
+},
+{
+  "name":"Bill",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      1,
+      1,
+      4,
+      4,
+      1,
+      1,
+      2,
+      1,
+      4,
+      1
+    ]
+},
+{
+  "name":"Ben",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      2,
+      1,
+      4,
+      5,
+      5,
+      1,
+      2,
+      5,
+      5,
+      1
+    ]
+},
+{
+  "name":"Betty",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      4,
+      1,
+      4,
+      4,
+      5,
+      1,
+      4,
+      5,
+      4,
+      4
+    ]
+},
+{
+  "name":"Betsy",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      3,
+      1,
+      4,
+      3,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"Berry",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      5,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      5
+    ]
+},
+{
+  "name":"Becca",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      3,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      3
+    ]
+},
+{
+  "name":"Belle",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      2,
+      5,
+      1,
+      2,
+      5,
+      4,
+      2
+    ]
+},
+{
+  "name":"Brook",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+
+];
