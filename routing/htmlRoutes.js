4f71a285db74a6145a5b395c05e1926d212701e6
zrcarter2017 <zcarter@allegiancy.us>
2018-02-07 23:50:53 -0500

    fixed html routes and added html post request

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,7 +1,13 @@
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
+module.exports = function(app) {
 
 app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "survey.html"));
+  res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
+
+
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+};
