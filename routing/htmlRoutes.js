14bbd7668a2f938da1bf52161e54398b8c69bb5f
zrcarter2017 <zcarter@allegiancy.us>
2018-02-07 23:51:41 -0500

    changed route to catchall

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -6,7 +6,7 @@ app.get("/survey", function(req, res) {
 
 
 
-app.get("/", function(req, res) {
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
