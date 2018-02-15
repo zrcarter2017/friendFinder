918a245e8fc0f76eb0b8ef54880e15e38c29fc43
zrcarter2017 <zcarter@allegiancy.us>
2018-02-15 18:29:56 -0500

    changed survey route

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -3,7 +3,7 @@ var path = require("path");
 module.exports = function(app) {
 
 app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "../app/public/survey.html"));
+  res.sendFile(path.join(__dirname, "../app/survey.html"));
 });
 
 app.get("/", function(req, res) {
